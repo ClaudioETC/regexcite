b80cee83f24250bbc22f99de30f91c0042061e26
ClaudioETC <claudio.torrescu@udlap.mx>
2022-04-03 22:03:13 -0700

    added function strsplit1 with test example

---
+++ R/strsplit1.R
@@ -0,0 +1,6 @@
+strsplit1 <- function(x, split) {
+  strsplit(x, split = split)[[1]]
+}
+(x <- "alfa,bravo,charlie,delta")
+strsplit1(x, split = ",")
+exists("strsplit1", where = globalenv(), inherits = FALSE)
